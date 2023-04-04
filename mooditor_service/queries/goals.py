8eabee9b406c48522ce47c4b1c9c6d7af9b6b57b
Jackie <jwliu24@gmail.com>
2023-04-04 15:19:56 -0700

    commiting to merge with main

--- mooditor_service/queries/goals.py
+++ mooditor_service/queries/goals.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
-from typing import Union
+from typing import Union, Optional
 from queries.pool import pool
+import datetime
 
 
 class Error(BaseModel):
@@ -10,13 +11,16 @@ class Error(BaseModel):
 class GoalIn(BaseModel):
     user_id: int
     goal: str
+    created_on: datetime.date
+    is_completed: Optional[bool] = False
 
 
 class GoalOut(BaseModel):
     id: int
     user_id: int
     goal: str
-
+    created_on: datetime.date
+    is_completed: Optional[bool] = False
 
 class GoalRepository:
     def create(self, goal: GoalIn) -> Union[GoalOut, Error]:
@@ -28,21 +32,25 @@ class GoalRepository:
                         INSERT INTO goals
                             (
                                 user_id,
-                                goal
+                                goal,
+                                created_on,
+                                is_completed
                             )
                         VALUES
-                            (%s, %s)
-                        RETURNING id;
+                            (%s, %s, %s, %s)
+                        RETURNING id, created_on;
                         """,
                         [
                             goal.user_id,
                             goal.goal,
+                            goal.created_on,
+                            goal.is_completed,
                         ]
                     )
                     id = result.fetchone()[0]
                     return self.goal_in_to_out(id, goal)
         except Exception:
-            return {"message": "Create goal did not work"}
+            return {"message": "Create goal did not work1"}
 
     def goal_in_to_out(self, id: int, goal: GoalIn):
         old_data = goal.dict()
@@ -51,6 +59,8 @@ class GoalRepository:
     def record_to_goal_out(self, record):
         return GoalOut(
             id=record[0],
-            goal=record[1],
-            user_id=record[2],
+            user_id=record[1],
+            goal=record[2],
+            created_on=record[3],
+            is_completed=record[4],
         )
