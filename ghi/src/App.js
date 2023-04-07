6498de7f2da925c34bd94d8bab2100b6b734a4ae
Jackie <jwliu24@gmail.com>
2023-04-06 18:10:19 -0700

    commiting changes

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -8,29 +8,6 @@ import SignupForm from "./SignupForm.js";
 import LoginForm from "./LoginForm.js";
 
 function App() {
-  const [launch_info, setLaunchInfo] = useState([]);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    async function getData() {
-      let url = `${process.env.REACT_APP_SAMPLE_SERVICE_API_HOST}/api/launch-details`;
-      console.log('fastapi url: ', url);
-      let response = await fetch(url);
-      console.log("------- hello? -------");
-      let data = await response.json();
-
-      if (response.ok) {
-        console.log("got launch data!");
-        setLaunchInfo(data.launch_details);
-      } else {
-        console.log("drat! something happened");
-        setError(data.message);
-      }
-    }
-    getData();
-  }, [])
-
-
   return (
     <BrowserRouter>
       <AuthProvider
@@ -46,38 +23,3 @@ function App() {
 }
 
 export default App;
-
-
-// function App() {
-//   const [launch_info, setLaunchInfo] = useState([]);
-//   const [error, setError] = useState(null);
-
-//   useEffect(() => {
-//     async function getData() {
-//       let url = `${process.env.REACT_APP_SAMPLE_SERVICE_API_HOST}/api/launch-details`;
-//       console.log('fastapi url: ', url);
-//       let response = await fetch(url);
-//       console.log("------- hello? -------");
-//       let data = await response.json();
-
-//       if (response.ok) {
-//         console.log("got launch data!");
-//         setLaunchInfo(data.launch_details);
-//       } else {
-//         console.log("drat! something happened");
-//         setError(data.message);
-//       }
-//     }
-//     getData();
-//   }, [])
-
-
-//   return (
-//     <div>
-//       <ErrorNotification error={error} />
-//       <Construct info={launch_info} />
-//     </div>
-//   );
-// }
-
-// export default App;
--- ghi/src/index.js
+++ ghi/src/index.js
@@ -1,23 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { AuthProvider } from "@galvanize-inc/jwtdown-for-react";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-import "./index.css";
-import reportWebVitals from "./reportWebVitals";
-
-import App from "./App";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/">
-      <AuthProvider
-        tokenUrl={`${process.env.REACT_APP_USER_SERVICE_API_HOST}/token`}
-      >
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
 
