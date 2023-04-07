f41200fbe23d6a36845cbb09735852213d9ba23b
Terence Wong <terencesw17@yahoo.com>
2023-04-07 00:33:41 +0000

    Login/Signup Form-Auth #30

---
+++ ghi/src/SignupForm.js
@@ -0,0 +1,84 @@
+import React, { useState, useContext } from "react";
+
+function SignupForm() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const data = {};
+    data.username = username;
+    data.email = email;
+    data.password = password;
+    const url = "http://localhost:8000/api/users";
+    const fetchConfig = {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    const response = await fetch(url, fetchConfig);
+    if (response.ok) {
+      await response.json();
+      setUsername("");
+      setEmail("");
+      setPassword("");
+    } else {
+      console.error("Could not create user");
+    }
+  };
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-12 col-md-4">
+          <div className="shadow p-4 mt-4">
+            <h1>Sign Up</h1>
+            <form onSubmit={handleSubmit}>
+              <div className="form-floating mb-3">
+                <input
+                  type="text"
+                  className="form-control"
+                  id="username"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  required
+                  placeholder="username"
+                />
+                <label htmlFor="username">Username</label>
+              </div>
+              <div className="form-floating mb-3">
+                <input
+                  type="text"
+                  className="form-control"
+                  id="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                  placeholder="email"
+                />
+                <label htmlFor="username">Email</label>
+              </div>
+              <div className="form-floating mb-3">
+                <input
+                  type="password"
+                  className="form-control"
+                  id="password"
+                  placeholder="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
+                <label htmlFor="password">Password</label>
+              </div>
+              <button className="btn btn-primary">Sign Up</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SignupForm;
