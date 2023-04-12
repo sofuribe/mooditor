6a128f02b62b74ab97d60ccbfba6109127f40990
Sofia Uribe <cesofia2013@gmail.com>
2023-04-12 20:16:09 +0000

    GET entries unit test passing- #38

---
+++ mooditor_service/tests/test_get_entries.py
@@ -0,0 +1,30 @@
+from fastapi.testclient import TestClient
+from main import app
+from queries.entries import EntriesRepo
+from authenticator import authenticator
+
+client = TestClient(app)
+
+class EmptyEntryQueries:
+    def get_entries(self, id):
+        return []
+
+test_user = {
+    "id": 1,
+    "username": "string",
+    "email": "string"
+}
+
+def user_override():
+    return test_user
+
+def test_get_all_entries():
+    app.dependency_overrides[EntriesRepo] = EmptyEntryQueries
+    app.dependency_overrides[authenticator.try_get_current_account_data] = user_override
+
+    response = client.get("/entries")
+
+    app.dependency_overrides = {}
+
+    assert response.status_code == 200
+    assert response.json() == []
