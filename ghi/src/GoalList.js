581b45e5949099c9fdc44fc75dcb32d501737884
Jackie <jwliu24@gmail.com>
2023-04-18 18:53:50 -0700

    testing pipeline

--- ghi/src/GoalList.js
+++ ghi/src/GoalList.js
@@ -1,19 +1,40 @@
 import React, { useEffect, useState } from 'react';
+import useToken from '@galvanize-inc/jwtdown-for-react';
 
 function GoalList() {
+    const { token } = useToken();
+
     const [goals, setGoals] = useState([]);
 
-    const fetchData = async () => {
-        const goalUrl = `${process.env.REACT_APP_USER_SERVICE_API_HOST}/goals`;
-        const response = await fetch(goalUrl);
+    useEffect(() => {
+        const fetchData = async () => {
+            const goalUrl = `${process.env.REACT_APP_USER_SERVICE_API_HOST}/goals`;
+            const fetchConfig = {
+                method: "put",
+                header: {
+                    Authorization: `Bearer ${token}`,
+                }
+            }
+            const response = await fetch(goalUrl);
 
-        if (response.ok) {
-            const data = await response.json();
-            setGoals(data.goals)
-        }
+            if (response.ok) {
+                const data = await response.json();
+                setGoals(data.goals)
+            }
+    }
+    })
+
+
+
+    async function deleteGoal(goal) {
+        const cancelUrl = `${process.env.REACT_APP_USER_SERVICE_API_HOST}/goal/`
     }
 
-    useEffect(() => {
-        fetchData();
-    }, []);
+    return (
+        <>
+
+        </>
+    )
 }
+
+export default GoalList;
