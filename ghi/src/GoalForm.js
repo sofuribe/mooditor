e07f7fdd439eed63a09af9d5fe9a5702c4a326da
Jackie <jwliu24@gmail.com>
2023-04-24 15:43:43 -0700

    merging with main

--- ghi/src/GoalForm.js
+++ ghi/src/GoalForm.js
@@ -72,7 +72,7 @@ function GoalForm () {
                 pauseOnFocusLoss={false}
                 draggable
                 pauseOnHover={false}
-                 />
+            />
 
         </div>
 
