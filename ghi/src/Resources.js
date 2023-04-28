284b34b7f48b2a5010dc5f23fd11f7ab9f8b21d2
Brooke <brooke.crockett6@gmail.com>
2023-04-28 15:18:20 -0700

    added resources page

---
+++ ghi/src/Resources.js
@@ -0,0 +1,30 @@
+import React from "react";
+// import { Link} from "react-router-dom";
+
+function Resources() {
+    return (
+    <>
+        <div>
+            <p className="m-20">
+                <a href="https://www.psychologytoday.com/us/therapists" target="_blank">Find a therapist near you on Psychology Today</a>
+            </p>
+        </div>
+        <div>
+            <p className="m-20">
+                <a href="https://988lifeline.org/" target="_blank">988 Crisis Lifeline</a>
+            </p>
+        </div>
+        <div>
+            <p className="m-20">
+                <a href="https://www.crisistextline.org/"target="_blank" >Call or text a crisis counselor for free</a>
+            </p>
+        </div>
+        <div>
+            <p className="m-20">
+                <a href="https://jedfoundation.org/mental-health-resource-center/"target="_blank" >Mental health resource center</a>
+            </p>
+        </div>
+    </>
+    );
+}
+export default Resources;
